13e3cc6612fb154792cc65e5b9bf19a189e7f16c
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-04-20 13:27:17 +0200

    first commit

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Utilities;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        
+
+        
+        static void Main(string[] args)
+        {
+
+            TextUtil myUtility = new TextUtil();
+            string stringToReverse = "racecars";
+
+            string stringToDisplay = myUtility.ReverseString(stringToReverse); //TextUtil is the static name
+            Console.WriteLine(stringToDisplay);
+
+            bool palindrome = myUtility.IsPalindrome(stringToReverse);
+
+            if (palindrome)
+                Console.WriteLine($"{stringToReverse} is a palindrome");
+            else
+                Console.WriteLine($"{stringToReverse} is not a palindrome");
+
+            /*   for(int counter =10;counter >= 0; counter--)
+               {
+                   Console.WriteLine(counter);
+               }*/
+
+            string stringToDisplays = myUtility.ReverseStringVersion2(stringToReverse);
+            Console.WriteLine(stringToDisplays);
+        }
+    }
+}
---
+++ Assignment 1/TextUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class TextUtil
+    {
+
+        public string ReverseString(string inputString)
+        {
+            string reversedString = string.Empty;
+
+            for (int counter = inputString.Length - 1; counter >= 0; counter--)
+            {
+                // Console.WriteLine(InputString[counter]);//Look at each char []-means array-string is in array of characters
+                reversedString += inputString[counter];
+                // reversedString = reversedString + InputString[counter];
+            }
+
+            return reversedString;
+        }
+        public string ReverseStringVersion2(string inputString)
+        {
+            string reversedString = string.Empty;
+
+            int counter = inputString.Length - 1;
+
+            while (counter >= 0)
+            {
+                reversedString += inputString[counter];
+                counter--;
+            }
+
+            return reversedString;
+        }
+
+        public bool IsPalindrome(string inputString)
+        {
+            string reversedString = ReverseStringVersion2(inputString);
+
+            if (inputString == reversedString)
+                return true;
+            else
+                return false;
+        }
+    }
+}
